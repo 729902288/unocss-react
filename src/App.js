c9f847b7a0dce4fd736953b2fdd2b5cc52510234
yxb <729902288@qq.com>
2022-01-13 10:39:16 +0800

    demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="preserve-3d perspect-900">
+      <div
+        className="w-20 h-20 m-auto duration-1000"
+        bg="blue-400"
+        hover="rotate-x-45 translate-x-30"
+      ></div>
     </div>
   );
 }
